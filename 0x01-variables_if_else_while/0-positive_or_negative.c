8901d36635aeb0b2a02b874a01d5a0c685d7739a
Ahmad M <ahmademohamed101@gmail.com>
2023-12-27 01:26:57 -0800

    modify lo lvl prgrmn:0x01.C Tasks 00 style and docs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,12 +5,17 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ *main - assign a random no and checks
+ *if it is positive, zero or negative
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	/* your code goes there */
 	if (n > 0)
 		printf("%u is positive\n", n);
