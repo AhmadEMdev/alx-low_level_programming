250daf5dca059000ffafbe0b84fb295158c0817b
Ahmad M <ahmademohamed101@gmail.com>
2023-12-28 04:32:53 -0800

    fix 0x01.C Tasks 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 
 /* betty style doc for function main goes there */
 /**
- *main - Prints all possible combinations of two different digits,
+ *main - Prints all possible combinations of two two-digit numbers,
  *in ascending order, separated by a comma followed by a space.
  *followed by a new line.
  *Return: Always 0 (Success)
@@ -18,20 +18,30 @@ int main(void)
 	/* your code goes there */
 	for (n01 = 48; n01 < 58; n01++)
 	{
-		for (n02 = n01; n02 < 58; n02++)
+		for (n02 = 48; n02 < 58; n02++)
 		{
-			/* if (n02 == n01)
-				continue; */
-			for(n11 = 48; n11 < 58; n11++) {
-				for(n12 = n11; n12 < 58; n12++) {
-					if (n12 == n11)
-						continue;
+
+			/*
+			 *if (n02 == n01)
+			 *continue;
+			 */
+
+			for (n11 = 48; n11 < 58; n11++)
+			{
+				for (n12 = 48; n12 < 58; n12++)
+				{
+
+					/*
+					 *if (n12 == n11)
+					 *continue;
+					 */
+
 					putchar(n01);
 					putchar(n02);
 					putchar(' ');
 					putchar(n11);
 					putchar(n12);
-					if (n01 == 57 && n02 ==  57 && n11 == 56 && n12 == 57)
+					if (n01 == 57 && n02 ==  56 && n11 == n12 == 57)
 						continue;
 					putchar(',');
 					putchar(' ');
